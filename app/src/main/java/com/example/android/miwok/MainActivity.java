a01bbff25cd59c0d427dfd3cb555705e5e185fc3
Raiyan <raiyan.sharif.1234@gmail.com>
2019-06-02 17:28:18 +0600

    onClickListener added to MainActivity instead of onClick in XML

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,24 +31,51 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
-    public void openNumberList(View view){
-        Intent i = new Intent(this, NumberActivity.class);
-        startActivity(i);
+
+
+        //setting click listener to numbers text view
+        TextView numberListTV = (TextView) findViewById(R.id.numbers);
+        //embedding interface for click listener
+        numberListTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(v.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity.this, NumberActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //family activity
+        TextView familyListTV = (TextView) findViewById(R.id.family);
+        familyListTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        //color text vew is being set with onClickListener
+        TextView colorListTV = (TextView) findViewById(R.id.colors);
+        colorListTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //phrase text view is being set with onClickListener
+        TextView phraseListTV = (TextView) findViewById(R.id.phrases);
+        phraseListTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
-    public void openFamilyList(View view){
-        Intent i = new Intent(this, FamilyActivity.class);
-        startActivity(i);
-    }
-
-    public void openColorList(View view){
-        Intent i = new Intent(this, ColorsActivity.class);
-        startActivity(i);
-    }
-
-    public void openPhraseList(View view){
-        Intent i = new Intent(this, PhrasesActivity.class);
-        startActivity(i);
-    }
 }
