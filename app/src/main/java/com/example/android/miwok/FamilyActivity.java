64b8ef8f3eee36e9ef5bd6a4e931c079188b6aa9
Raiyan <raiyan.sharif.1234@gmail.com>
2019-06-09 01:27:38 +0600

    all audio works in family activity

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,19 +1,23 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> numberWord = new ArrayList<Word>();
+        final ArrayList<Word> numberWord = new ArrayList<Word>();
         numberWord.add(new Word("father","әpә", R.drawable.family_father, R.raw.family_father));
         numberWord.add(new Word("mother","әṭa", R.drawable.family_mother, R.raw.family_mother));
         numberWord.add(new Word("son","angsi", R.drawable.family_son, R.raw.family_son));
@@ -30,11 +34,14 @@ public class FamilyActivity extends AppCompatActivity {
 
         listView.setAdapter(itemsAdapter);
 
-//        for(int i=0; i<numberWord.size();i++){
-//            TextView wordView = new TextView(this);
-//            wordView.setText(numberWord.get(i));
-//            numbersRootView.addView(wordView);
-//        }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word word = numberWord.get(position);
+                mMediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getAudioResourceId());
+                mMediaPlayer.start();
+            }
+        });
 
 
 
