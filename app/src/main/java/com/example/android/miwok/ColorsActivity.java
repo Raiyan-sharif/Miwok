f86624b3c325ddd3b33dc0fe43578e0454a069aa
Raiyan <raiyan.sharif.1234@gmail.com>
2019-06-26 23:48:05 +0600

    colors fragment added

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -12,98 +12,13 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
-    private MediaPlayer mMediaPlayer;
-
-    private AudioManager mAudioManager;
-
-    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
-            new AudioManager.OnAudioFocusChangeListener() {
-                @Override
-                public void onAudioFocusChange(int focusChange) {
-                    if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                            focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
-                        mMediaPlayer.pause();
-                        mMediaPlayer.seekTo(0);
-
-                    }
-                    else if(focusChange == AudioManager.AUDIOFOCUS_GAIN){
-                        mMediaPlayer.start();
-
-                    }
-                    else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
-                        releaseMediaPlayer();
-                    }
-                }
-            };
-
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releaseMediaPlayer();
-        }
-    };
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-
-        final ArrayList<Word> numberWord = new ArrayList<Word>();
-        numberWord.add(new Word("red","weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
-        numberWord.add(new Word("green","chokokki", R.drawable.color_green, R.raw.color_green));
-        numberWord.add(new Word("brown","ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
-        numberWord.add(new Word("gray","ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
-        numberWord.add(new Word("black","kululli", R.drawable.color_black, R.raw.color_black));
-        numberWord.add(new Word("white","kelelli", R.drawable.color_white, R.raw.color_white));
-        numberWord.add(new Word("dusty yellow","ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
-        numberWord.add(new Word("mustard yellow","chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
-
-        WordAdapter itemsAdapter = new WordAdapter(this,numberWord, R.color.category_colors);
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(itemsAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Word word = numberWord.get(position);
-                releaseMediaPlayer();
-                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceId());
-                    mMediaPlayer.start();
-                    //Setup a listener on the media player, so that we can stop and release the
-                    //media player once the sounds has finished playing.
-                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                }
-            }
-        });
-
-
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ColorsFragment()).commit();
 
     }
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mMediaPlayer.release();
 
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-        }
-    }
 }
