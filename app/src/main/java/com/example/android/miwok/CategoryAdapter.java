a60568998ce0a8ec3736ee9c86542f7b05aa113f
Raiyan <raiyan.sharif.1234@gmail.com>
2019-06-27 00:41:52 +0600

    pagination added

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,29 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+    public CategoryAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -33,48 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //setting click listener to numbers text view
-        TextView numberListTV = (TextView) findViewById(R.id.numbers);
-        //embedding interface for click listener
-        numberListTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Toast.makeText(v.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(MainActivity.this, NumberActivity.class);
-                startActivity(i);
-            }
-        });
-
-        //family activity
-        TextView familyListTV = (TextView) findViewById(R.id.family);
-        familyListTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(i);
-            }
-        });
-
-
-        //color text vew is being set with onClickListener
-        TextView colorListTV = (TextView) findViewById(R.id.colors);
-        colorListTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(i);
-            }
-        });
-
-        //phrase text view is being set with onClickListener
-        TextView phraseListTV = (TextView) findViewById(R.id.phrases);
-        phraseListTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(i);
-            }
-        });
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
     }
 
